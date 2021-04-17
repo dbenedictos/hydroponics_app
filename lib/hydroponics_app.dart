850999c8e34980aecdbd72dc46872836bcba15c8
Dominic Benedictos <dominic.benedictos@ffufm.com>
2021-04-17 19:07:07 +0800

    Initial Setup
    - package structure

---
+++ lib/hydroponics_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:hydroponics_app/feature/home_page.dart';
+
+class HydroponicsApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:hydroponics_app/main.dart';
+import 'package:hydroponics_app/hydroponics_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(HydroponicsApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
