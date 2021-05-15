4aafba4f860a83ebe760713a5fdf23bc6e6df0ce
Dominic Benedictos <vbenedictos22@gmail.com>
2021-05-15 19:14:40 +0800

    add image

--- lib/feature/home/home_page.dart
+++ lib/feature/home/home_page.dart
@@ -4,11 +4,21 @@ import 'package:hydroponics_app/feature/widgets/sidebar.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
         child: Row(
           children: [
             SideBar(),
+            Expanded(
+              child: Center(
+                child: Image.asset(
+                  'assets/sheila.png',
+                  fit: BoxFit.fill,
+                  height: screenSize.height,
+                ),
+              ),
+            ),
           ],
         ),
       ),
